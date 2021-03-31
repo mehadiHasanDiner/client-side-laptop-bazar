a10a3cf5e90c1389afed75b18315f062f91a8c6e
mehadi-500 <mehadi.cosmo@gmail.com>
2021-03-31 16:35:59 +0600

    admin form for product created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button} from 'react-bootstrap';
+import Admin from "./Components/Admin/Admin";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
-    <div className ="container">
-      <h2>React Bootstrap</h2>
-      <Button variant="primary">Button</Button>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/admin">
+          <Admin></Admin>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/Admin/Admin.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Col, Container, Button } from 'react-bootstrap';
+// import { Button } from 'bootstrap';
+
+const Admin = () => {
+    return (
+        <Container>
+            <Form className = "p-3" style={{backgroundColor:'#e1e1e1'}}>
+            <h3>Add Product</h3>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label><strong>Product Name</strong></Form.Label>
+                        <Form.Control type="text" placeholder="Enter Product Name" />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridPassword">
+                        <Form.Label><strong>Brand Name</strong></Form.Label>
+                        <Form.Control type="text" placeholder="Enter Brand" />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label><strong>Add Price</strong></Form.Label>
+                        <Form.Control type="number" placeholder="Enter Price" />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridPassword">
+                        <Form.Label><strong>Add Price</strong></Form.Label>
+                        <Form.File className = "p-1"  id="exampleFormControlFile1"/>
+                    </Form.Group>
+                </Form.Row>
+                <Button variant="primary" type="submit">Save  </Button>
+            </Form>
+        </Container>
+    );
+};
+
+export default Admin;
